1f0040e1b6775e7ec9ae905c68a0e571c3470b5d
Robert Jackson <dwraithrah@gmail.com>
2020-03-01 10:48:50 -0600

    cleanup

--- CEGameApp.API/Dtos/UserForRegisterDto.cs
+++ CEGameApp.API/Dtos/UserForRegisterDto.cs
@@ -5,6 +5,7 @@ namespace CEGameApp.API.Dtos
     public class UserForRegisterDto
     {
         [Required]
+        [StringLength(16, MinimumLength = 4, ErrorMessage="You must enter a user name between 4 and 16 characters long")]
         public string UserName { get; set; }
 
         [Required]
